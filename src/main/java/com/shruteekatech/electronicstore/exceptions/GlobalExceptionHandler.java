a6cf7d2dd5852301f694de6938c2ddef4d561aa3
sagarpadwekar <imsagarpadwekar@gmail.com>
2023-06-08 09:42:19 +0530

    Loggers added in GlobalExceptionHandler.java

--- src/main/java/com/shruteekatech/electronicstore/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/shruteekatech/electronicstore/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.shruteekatech.electronicstore.exceptions;
 
 
 import com.shruteekatech.electronicstore.helper.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,12 +11,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiResponse> resourceNotFound(ResourceNotFoundException e) {
+        log.error("ResourceNotFoundException Occurred , Resource is not available in database");
         return new ResponseEntity<>(ApiResponse.builder().message(e.getMessage())
                 .status(HttpStatus.NOT_FOUND).success(false).build(), HttpStatus.NOT_FOUND);
     }
@@ -23,6 +25,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> nonValidArgs(MethodArgumentNotValidException e) {
         Map<String, String> map = new HashMap<>();
+        log.error("ArgumentNotValidException , please provide valid arguments");
         e.getBindingResult().getFieldErrors().forEach(fieldError ->
                         map.put(fieldError.getField(), //getting fields which have errors
                                 fieldError.getDefaultMessage()) //error messages
