79ff4dec72bb3879537c2ac2b1dd26ea0f63662e
sagarpadwekar <imsagarpadwekar@gmail.com>
2023-07-24 20:02:12 +0530

    InvalidFileException.java class is created , for handling exception due to invalid image file selection

---
+++ src/main/java/com/shruteekatech/electronicstore/exceptions/InvalidFileException.java
@@ -0,0 +1,8 @@
+package com.shruteekatech.electronicstore.exceptions;
+
+public class InvalidFileException extends RuntimeException {
+    public InvalidFileException(String message) {
+        super(message);
+    }
+
+}
