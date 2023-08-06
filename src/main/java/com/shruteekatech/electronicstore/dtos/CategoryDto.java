8ebdc311301f4434c3a6a603d7b1cabdf31a1c41
sagarpadwekar <imsagarpadwekar@gmail.com>
2023-08-06 08:15:13 +0530

    Category Dto created

--- src/main/java/com/shruteekatech/electronicstore/dtos/CategoryDto.java
+++ src/main/java/com/shruteekatech/electronicstore/dtos/CategoryDto.java
@@ -2,6 +2,9 @@ package com.shruteekatech.electronicstore.dtos;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -9,7 +12,10 @@ import lombok.*;
 @Builder
 public class CategoryDto {
     private String id;
+    @NotBlank(message = "Title cannot be blank")
     private String title;
+    @NotBlank(message = "Description cannot be blank")
+    @Size(min = 10, message = "Description must be at least 10 characters long")
     private String description;
     private String coverImage;
 }
