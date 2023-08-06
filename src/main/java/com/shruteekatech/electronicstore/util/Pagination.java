1e62bc967794013cec8ee575428fa817b7a76c14
sagarpadwekar <imsagarpadwekar@gmail.com>
2023-08-06 08:21:05 +0530

    Pagination class updated , from() method added for sorting and generating pages using

--- src/main/java/com/shruteekatech/electronicstore/util/Pagination.java
+++ src/main/java/com/shruteekatech/electronicstore/util/Pagination.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+
 import java.util.List;
 
 @Getter
@@ -24,11 +25,13 @@ public class Pagination<T> {
     public static Pageable from(Integer pageNo, Integer pageSize, String order, String sortUsing) {
         Sort sort = (order.equalsIgnoreCase("desc")) ?
                 Sort.by(sortUsing).descending() : Sort.by(sortUsing).ascending();
+
         return PageRequest.of(pageNo, pageSize, sort);
     }
 
-    public static <T, E> Pagination<T> of(Page<E> page,Class<T> dtoClass) {
-        Page<T> dtoPage = page.map(entity -> new ModelMapper().map(entity,dtoClass));
+    public static <T, E> Pagination<T> of(Page<E> page, Class<T> dtoClass) {
+        Page<T> dtoPage = page.map(entity -> new ModelMapper().map(entity, dtoClass));
+
         return Pagination.<T>builder()
                 .content(dtoPage.getContent())
                 .pageNo(dtoPage.getNumber())
