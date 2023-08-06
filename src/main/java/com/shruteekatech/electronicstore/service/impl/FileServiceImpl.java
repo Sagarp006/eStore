5492b3ed7c8ef73badccf2ed4a0e40d92bdf8174
sagarpadwekar <imsagarpadwekar@gmail.com>
2023-08-06 08:18:33 +0530

    FileServiceImpl.java created for image processing

--- src/main/java/com/shruteekatech/electronicstore/service/impl/FileServiceImpl.java
+++ src/main/java/com/shruteekatech/electronicstore/service/impl/FileServiceImpl.java
@@ -1,7 +1,7 @@
 package com.shruteekatech.electronicstore.service.impl;
 
+import com.shruteekatech.electronicstore.exceptions.InvalidFileException;
 import com.shruteekatech.electronicstore.exceptions.ResourceNotFoundException;
-import com.shruteekatech.electronicstore.helper.AppConstants;
 import com.shruteekatech.electronicstore.service.FileService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,37 +11,45 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.UUID;
-import java.util.function.UnaryOperator;
 
 
-@Service
 @Slf4j
+@Service
 public class FileServiceImpl implements FileService {
+
     @Override
-        public String uploadFile(MultipartFile file, String path) throws IOException {
-            log.info("image is being uploaded");
+    public String uploadFile(MultipartFile imageFile, String path) throws IOException {
+        String ext = getExt(imageFile);
 
-            String ext = getExt.apply(file.getOriginalFilename());
+        // Check if the file extension matches .png, .jpg, or .jpeg
+        if (ext.matches("\\.(png|jpg|jpeg)$")) {
+            // Generate a unique file name with the extension (e.g., 83c3n3-m84mv.png)
+            String fileNameWithExtension = UUID.randomUUID() + ext;
 
-            if (ext.matches("\\.(png|jpg|jpeg)$")) {
-                //generating imageName eg:- 7yjw93-j94093.png
-                String imageName = UUID.randomUUID() + ext;
+            // Combine the specified image path with the generated file name and extension
+            String fullPathWithFileName = path + fileNameWithExtension;
+            log.info("Request for upload user image to the image path: {}", fullPathWithFileName);
 
-                //generating path eg:- image/category/7yjw93-j94093.png
-                String imageFileWithPathName = path + imageName;
+            // Create the directory if it doesn't exist
+            Files.createDirectories(Paths.get(path));
 
-                //uploading file to a given path
-                Files.copy(file.getInputStream(), Paths.get(imageFileWithPathName));
-                log.info("image uploaded successfully");
-                return imageName;
-            } else {
-                log.info("Selected File is Not an Image");
-                throw new ResourceNotFoundException(AppConstants.IMG_NA);
-            }
+            // Copy the file's content to the specified full path
+            Files.copy(imageFile.getInputStream(), Paths.get(fullPathWithFileName));
+
+            log.info("Request Completed for upload user image");
+
+            // Return the generated file name with extension
+            return fileNameWithExtension;
+        } else {
+            log.error("Invalid file format: {}", ext);
+            throw new InvalidFileException("Invalid file format. Only .png, .jpg, and .jpeg files are allowed.");
         }
+    }
+
+
     @Override
     public InputStream getResource(String path, String name) {
-        String fullPath = path.concat(File.separator).concat(name);
+        String fullPath = path + File.separator + name;
         try {
             return new FileInputStream(fullPath);
         } catch (FileNotFoundException e) {
@@ -49,12 +57,25 @@ public class FileServiceImpl implements FileService {
         }
     }
 
-    // @returns image extension eg:- .png
-    private final UnaryOperator<String> getExt = originalFilename -> {
-        if (originalFilename == null || originalFilename.isEmpty()) {
-            throw new ResourceNotFoundException("File is empty");
+    private static String getExt(MultipartFile file) {
+        if (file.isEmpty()) {
+            log.error("File is empty");
+            throw new InvalidFileException("File is empty");
         }
-        return originalFilename.substring(originalFilename.lastIndexOf(".")).toLowerCase();
-    };
-}
 
+        String originalFilename = file.getOriginalFilename();
+
+        if (originalFilename == null || originalFilename.isEmpty()) {
+            log.error("File name is empty");
+            throw new InvalidFileException("File name is empty");
+        }
+
+        if (!originalFilename.contains(".")){
+            log.error("File does not contain any extension");
+            throw new InvalidFileException("File does not contain any extension");
+        }
+
+        return originalFilename.substring(originalFilename.lastIndexOf(".")).toLowerCase();
+    }
+
+}
