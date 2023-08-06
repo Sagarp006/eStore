3012925fca188bb66a2fa7bbb2942677eb675281
sagarpadwekar <imsagarpadwekar@gmail.com>
2023-08-06 08:16:03 +0530

    CategoryRepository interface created

---
+++ src/main/java/com/shruteekatech/electronicstore/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.shruteekatech.electronicstore.repositories;
+
+import com.shruteekatech.electronicstore.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, String> {
+    List<Category> findByTitleContaining(String keyword);
+}
