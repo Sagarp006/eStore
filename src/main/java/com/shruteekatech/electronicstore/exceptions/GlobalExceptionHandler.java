9dfc72642ccab834631423ec6d9b6cb94c41d41c
sagarpadwekar <imsagarpadwekar@gmail.com>
2023-08-06 08:23:22 +0530

    UnCheckedIOException handler is added

--- src/main/java/com/shruteekatech/electronicstore/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/shruteekatech/electronicstore/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.shruteekatech.electronicstore.exceptions;
 
+import com.shruteekatech.electronicstore.helper.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,42 +9,77 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.UncheckedIOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
+@SuppressWarnings(value = "unused")
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> resourceNotFound(ResourceNotFoundException e) {
-        log.error("ResourceNotFoundException Occurred , Resource is not available in database");
-        Map<String, Object> response = Map.of("message", e.getMessage(), "status", HttpStatus.NOT_FOUND);
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    public ResponseEntity<ApiResponse> resourceNotFound(ResourceNotFoundException e) {
+        log.error("ResourceNotFoundException occurred: {}", e.getMessage());
+
+        ApiResponse response = ApiResponse.builder()
+                .message(e.getMessage())
+                .status(HttpStatus.BAD_REQUEST)
+                .success(false)
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 
     @ExceptionHandler(InvalidFileException.class)
-    public ResponseEntity<Map<String, Object>> resourceNotFound(InvalidFileException e) {
-        Map<String, Object> response = Map.of("message", e.getMessage(), "status", HttpStatus.NOT_FOUND);
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    public ResponseEntity<Map<String, Object>> invalidFile(InvalidFileException e) {
+        log.error("InvalidFileException occurred: {}", e.getMessage());
+
+        Map<String, Object> response = Map.of(
+                "message", e.getMessage(),
+                "status", HttpStatus.BAD_REQUEST
+        );
+
+        return ResponseEntity.ok(response);
     }
 
     @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
-    public ResponseEntity<String> mediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException e) {
-        log.error("The request handler cannot generate a acceptable response ");
-        return ResponseEntity.ok(e.getMessage());
+    public ResponseEntity<ApiResponse> mediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException e) {
+        log.error("HttpMediaTypeNotAcceptableException occurred: {}", e.getMessage());
+
+        ApiResponse response = ApiResponse.builder()
+                .message(e.getMessage())
+                .status(HttpStatus.BAD_REQUEST)
+                .success(false)
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @ExceptionHandler(UncheckedIOException.class)
+    public ResponseEntity<ApiResponse> unchecked(UncheckedIOException e) {
+        log.error("UncheckedIOException occurred: {}", e.getMessage());
+
+        ApiResponse response = ApiResponse.builder()
+                .message(e.getMessage())
+                .status(HttpStatus.OK)
+                .success(false)
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> nonValidArgs(MethodArgumentNotValidException e) {
-        Map<String, String> map = new HashMap<>();
-        log.error("ArgumentNotValidException Occurred , provide valid arguments");
+        log.error("MethodArgumentNotValidException occurred: valid arguments is not provided");
 
-        //getting all field errors from bindingResult and iterating using for loop
+        Map<String, String> map = new HashMap<>();
+
+        // Getting all field errors from BindingResult and iterating using a for loop
         e.getBindingResult().getFieldErrors().forEach(fieldError ->
-                map.put(fieldError.getField(), //getting fields which have errors
-                        fieldError.getDefaultMessage()) //error messages
-        );
+                map.put(fieldError.getField(), fieldError.getDefaultMessage())
+        );                 //fields               // error messages
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map);
     }
 }
